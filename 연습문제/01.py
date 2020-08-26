36f4bcc5a7f1f64cb68c5eb8500bb7a3995e6e30
codingpen.io <codingpen.io@gmail.com>
2020-08-26 15:57:10 +0900

    1번 풀이 추가

---
+++ 연습문제/01.py
@@ -0,0 +1,100 @@
+'''
+N개의 정수로 이루어진 배열이 주어진다. 이 배열의 최대구간합을 구하는 프로그램을 작성하시오.
+
+예를 들어, 배열이 [2, 5, -9, 7, 3, -2, 5] 인 경우, 
+네 번째 값부터 일곱 번째 값까지 해당하는 구간의 합은 1313이며 구간합 중 가장 큰 값이 되므로 
+최대구간합은 1313이 된다.
+
+입력 형식
+입력의 첫 줄에 배열의 크기 NN이 주어진다. (1 <= N <= 1 000 000)
+
+둘째 줄에 배열의 내용을 나타내는 NN개의 정수가 공백으로 구분되어 주어진다. 주어지는 정수의 절대값은 10^9보다 크지 않다.
+
+출력 형식
+첫 줄에 최대구간합을 출력한다.
+
+입력 예제
+```
+7
+2 5 -9 7 3 -2 5
+```
+
+출력예제
+```
+13
+```
+'''
+
+# # 방법 1 브루트 포스
+# n = int(input())
+# arr = list(map(int, input().split()))
+
+# max_sum = 0
+# for i in range(n):
+#     s = 0
+#     for j in range(i, n):
+#         s += arr[j]
+
+#     if s > max_sum:
+#         max_sum = s
+
+# print(max_sum)
+
+# 방법2 dynamic programming
+# 30점 짜리
+# n = int(input())
+# arr = list(map(int, input().split()))
+
+# memo = [None] * n
+# memo[0] = arr[0]
+
+# for i in range(1, n):
+#     memo[i] = max(0, memo[i-1]+arr[i])
+
+# print(max(memo))
+
+# 방법3
+# n = int(input())
+# arr = list(map(int, input().split()))
+
+# memo = [None] * n
+# memo[0] = arr[0]
+
+# myMax = memo[0]
+# for i in range(1, n):
+#     memo[i] = max(0, memo[i-1]+arr[i])
+#     if memo[i] > myMax:
+#         myMax = memo[i]
+
+
+# print(myMax)
+
+'''
+n = int(input())
+arr = list(map(int, input().split()))
+
+memo = [None] * n
+memo[0] = arr[0]
+
+myMax = memo[0]
+for i in range(1, n):
+    memo[i] = max(arr[i], memo[i-1]+arr[i])
+    # if memo[i] > myMax:
+    #     myMax = memo[i]
+    myMax = max(memo[i], myMax)
+
+
+print(myMax)
+
+'''
+
+n = int(input())
+arr = list(map(int, input().split()))
+
+myLastMax = myMax = arr[0]
+
+for a in arr[1:]:
+    myLastMax = max(a, myLastMax + a)
+    myMax = max(myLastMax, myMax)
+
+print(myMax)
